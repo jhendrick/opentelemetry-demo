c878b5298010d2c40556b2113af2a37edfb2d23f
Josh Hendrick <jhendrick@users.noreply.github.com>
2024-08-20 09:49:41 -0700

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,3 +3,6 @@
 # copy config to user dir to use helm 
 cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
 chown $(id -u):$(id -g) ~/.kube/config
+
+# deploy
+chmod +x .devcontainer/deployment.sh && .devcontainer/deployment.sh
