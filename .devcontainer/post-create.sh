c22df02a18dc549c379999c545e3c41b1bcbb2c4
Josh Hendrick <jhendrick@users.noreply.github.com>
2024-08-21 14:33:31 -0700

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-kind create cluster --config .devcontainer/kind-cluster.yml --wait 300s
-
-# deploy
-# chmod +x .devcontainer/deployment.sh && .devcontainer/deployment.sh
+kind create cluster --config .devcontainer/kind-cluster.yaml --wait 300s
