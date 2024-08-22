cfa2f69e14e5108f6773e53aba34b75dc57e8f18
Josh Hendrick <jhendrick@users.noreply.github.com>
2024-08-22 15:49:07 -0700

    Update deployment.sh

--- .devcontainer/deployment.sh
+++ .devcontainer/deployment.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
+if [ -z "${DT_TOKEN}" || -z "${DT_ENDPOINT}"]; then
+    echo "Required variables DT_TOKEN and DT_ENDPOINT are not set. Exiting..."
+    exit 1
+fi
+
+kind create cluster --config .devcontainer/kind-cluster.yaml --wait 300s
+
+# replace token and endpoint with user provided values
+sed -i -e 's/DT_TOKEN/'"$DT_TOKEN"'/g' dynakube.yaml 
+sed -i -e 's/DT_ENDPOINT/'"$DT_ENDPOINT"'/g' dynakube.yaml
+
+# install the Dynatrace operator
 helm install dynatrace-operator oci://public.ecr.aws/dynatrace/dynatrace-operator \
     --create-namespace \
     --namespace dynatrace \
     --atomic
 
+# Apply the Dynakube in ApplicationOnly mode
 kubectl apply -f dynakube.yaml
