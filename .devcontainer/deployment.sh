611085a45db40dd227475352ab5a142544faf060
Josh Hendrick <jhendrick@users.noreply.github.com>
2024-08-23 10:24:31 -0700

    Update deployment.sh

--- .devcontainer/deployment.sh
+++ .devcontainer/deployment.sh
@@ -7,6 +7,9 @@ fi
 
 kind create cluster --config .devcontainer/kind-cluster.yaml --wait 300s
 
+# create the secret with the user provided API token
+kubectl -n dynatrace create secret generic kind-k8s --from-literal="apiToken=$DT_TOKEN"
+
 # remove trailing slash on DT_ENDPOINT if it exists
 DT_ENDPOINT=$(echo "$DT_ENDPOINT" | sed "s,/$,,")
 echo "Removed trailing slashes in $DT_ENDPOINT"
@@ -19,9 +22,6 @@ helm install dynatrace-operator oci://public.ecr.aws/dynatrace/dynatrace-operato
     --create-namespace \
     --namespace dynatrace \
     --atomic
-
-# create the secret with the user provided API token
-kubectl -n dynatrace create secret generic kind-k8s --from-literal="apiToken=$DT_TOKEN"
 
 # Apply the Dynakube in ApplicationOnly mode
 kubectl apply -f .devcontainer/dynakube.yaml
