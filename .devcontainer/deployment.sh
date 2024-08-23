d548ff283840a1ab8c2d7450d79354fbc68359d3
Josh Hendrick <jhendrick@users.noreply.github.com>
2024-08-22 17:20:10 -0700

    Update deployment.sh

--- .devcontainer/deployment.sh
+++ .devcontainer/deployment.sh
@@ -7,6 +7,9 @@ fi
 
 kind create cluster --config .devcontainer/kind-cluster.yaml --wait 300s
 
+# remove trailing slash on DT_ENDPOINT if it exists
+DT_ENDPOINT=$(echo "$DT_ENDPOINT" | sed "s,/$,,")
+
 # replace token and endpoint with user provided values
 sed -i "s|DT_TOKEN|$DT_TOKEN|g" .devcontainer/dynakube.yaml 
 sed -i "s|DT_ENDPOINT|$DT_ENDPOINT|g" .devcontainer/dynakube.yaml
