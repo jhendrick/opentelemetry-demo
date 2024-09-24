bbef916e997007097ed14a9a7c98d4e09a3136bc
Josh Hendrick <jhendrick@users.noreply.github.com>
2024-09-24 15:55:45 -0700

    Update deployment.sh

--- .devcontainer/deployment.sh
+++ .devcontainer/deployment.sh
@@ -40,5 +40,5 @@ helm upgrade -i dynatrace-collector open-telemetry/opentelemetry-collector -f .d
 helm upgrade -i my-otel-demo open-telemetry/opentelemetry-demo -f .devcontainer/otel-demo-values.yaml
 
 # Wait for pods frontend and flagd pods to be ready before we use them
-kubectl rollout status deployment opentelemetry-demo-frontend
-kubectl rollout status deployment opentelemetry-demo-flagd
+kubectl rollout status deployment my-otel-demo-frontendproxy
+kubectl rollout status deployment my-otel-demo-flagd
